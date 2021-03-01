62fec81b11a22e9b5f9a45ec18386e5d830dc423
Jamal <jamensky1991@gmail.com>
2021-03-01 23:21:09 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,19 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles' do
-    @articles = Article.all 
-    erb :index
+   if params.empty?
+     @articles = Article.all 
+     erb :index
+   else  
+    redirect "articles/:id"
+    #binding.pry
+   end
   end
 
   get '/articles/:id' do
+    binding.pry
+    @requested_article = Article.find_by_id(params[:id])
+    erb :show
   end
 
 end
